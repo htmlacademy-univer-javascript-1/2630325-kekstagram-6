45dcedb967c446bf11221abfc505ce4ba14fc8cf
Веретенников Егор <etennikoff@gmail.com>
2025-11-16 13:12:59 +0500

    .

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,1 @@
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-  return Math.floor(Math.random() * (upper - lower + 1) + lower);
-};
 
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
-export { getRandomInteger, getRandomArrayElement };
