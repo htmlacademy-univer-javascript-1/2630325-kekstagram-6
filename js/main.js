3dca023e187e87d18ef9572deff885ae74d0f18c
Веретенников Егор <etennikoff@gmail.com>
2025-12-01 10:03:57 +0500

    wip main.js

--- js/main.js
+++ js/main.js
@@ -6,13 +6,15 @@ import './form.js';
 const initApp = () => {
   loadPictures()
     .then((pictures) => {
+      console.log('Получено фото:', pictures.length);
+      console.log('Первое фото:', pictures[0]);
       renderThumbnails(pictures);
     })
     .catch((error) => {
+      console.log('Ошибка загрузки:', error);
       showLoadError(error.message);
     });
 };
 
 document.addEventListener('DOMContentLoaded', initApp);
-
 
