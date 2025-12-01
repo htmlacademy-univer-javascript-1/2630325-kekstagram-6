75ae051a8d971c54b9cf729529e301a1d5314ef9
Веретенников Егор <etennikoff@gmail.com>
2025-12-01 10:55:13 +0500

    update main.js

--- js/main.js
+++ js/main.js
@@ -6,12 +6,9 @@ import './form.js';
 const initApp = () => {
   loadPictures()
     .then((pictures) => {
-      console.log('Получено фото:', pictures.length);
-      console.log('Первое фото:', pictures[0]);
       renderThumbnails(pictures);
     })
     .catch((error) => {
-      console.log('Ошибка загрузки:', error);
       showLoadError(error.message);
     });
 };
